a3bcc2b7d920c7f208f0ef47a35ce433b92ef331
Tien Thang Dinh <tienthangdinh@hotmail.com>
2025-02-17 12:54:29 +0100

    dataset class added

--- dataset.py
+++ dataset.py
@@ -14,10 +14,12 @@ class CarvanaDataset(Dataset):
         return len(self.images)
 
     def __getitem__(self, index):
-        img_path = os.path.join(self.image_dir, self.images[index])
+        img_path = os.path.join(self.image_dir, self.images[index]) #https://docs.python.org/3/library/os.path.html
         mask_path = os.path.join(self.mask_dir, self.images[index].replace(".jpg", "_mask.gif"))
-        image = np.array(Image.open(img_path).convert("RGB"))
-        mask = np.array(Image.open(mask_path).convert("L"), dtype=np.float32)
+
+        #read the images and convert to numpy array
+        image = np.array(Image.open(img_path).convert("RGB")) #nunpy.array https://numpy.org/doc/2.1/reference/generated/numpy.array.html
+        mask = np.array(Image.open(mask_path).convert("L"), dtype=np.float32) #https://pillow.readthedocs.io/en/stable/reference/Image.html
         mask[mask == 255.0] = 1.0
 
         if self.transform is not None:
